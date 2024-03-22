2b03e32012ec10d4af377a9bccf6879949307463
busayogabriel47 <omotukabusayo22@gmail.com>
2024-03-22 15:57:54 +0000

    Dashboard layout tailwind design

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -3,6 +3,9 @@ import graphtwo from "../../assets/Graph2.png"
 import graphthree from "../../assets/Graph3.png"
 import arrowup from "../../assets/arrow-trend-up.png"
 import walleticon from "../../assets/walleticon.png"
+import bluegraph from "../../assets/Bluegraph.png"
+import donutchart from "../../assets/Donut-Chart.png"
+import dot from "../../assets/Dot1.png"
 
 
 const Dashboard = () => {
@@ -79,6 +82,83 @@ const Dashboard = () => {
                   <div>
                       <img src={graphone} alt=''/>
                   </div>
+              </div>
+        </div>
+
+      {/* */}
+        <div className="grid grid-cols-2 gap-4 my-5">
+            <div className='flex flex-col gap-5 border-2 border-[#F1F1F1] p-10 rounded-2xl'>
+                    {/*Sales Analytics header*/}
+                      <div className="flex basis-[100%] justify-between">
+                            <h6>Sales Analytics</h6>
+                            <h3>Week</h3>
+                      </div>
+                      {/*Sales Analytics graph*/}
+                      <div className="w-full">
+                            <div className="pt-5">
+                                <img src={bluegraph} alt="bluegraph" width="100%"/>
+                            </div>
+                      </div>
+                      
+              </div>
+
+              <div className='flex flex-col gap-5 border-2 border-[#F1F1F1] p-10 rounded-2xl'>
+                    {/*Sales by category header*/}
+                      <div className="flex basis-[100%] justify-between">
+                            <h6>Sales Analytics</h6>
+                            <h3>Week</h3>
+                      </div>
+                      {/*Sales Analytics graph*/}
+                      <div className="flex flex-row w-full">
+                            <div className="pt-5 basis-[40%]">
+                                <img src={donutchart} alt="bluegraph" width="100%"/>
+                            </div>
+                            
+                            <div className="flex flex-col gap-4 basis-[60%]">
+                                  {/* Clothing*/}
+                                  <div className="flex flex-row">
+                                      <div className="w-[80%]">
+                                            <div className="flex gap-4 items-center">
+                                              <span>
+                                                <img src={dot} alt="dot"/>
+                                              </span>
+                                              <span>
+                                                <div>
+                                                  <p className="text-[1.2rem]">Clothing (25%)</p>
+                                                  <p className="text-[0.6rem]">1,345 Category Products</p>
+                                                </div>
+                                              </span>
+                                            </div>
+                                      </div>
+                                      <div className="w-[20%]">
+                                        <p>$3,020</p>
+                                      </div>
+                                  </div>
+
+                                  {/* Lightwear*/}
+                                  <div className="flex flex-row">
+                                      <div className="w-[80%]">
+                                            <div className="flex gap-4 items-center">
+                                              <span>
+                                                <img src={dot} alt="dot"/>
+                                              </span>
+                                              <span>
+                                                <div>
+                                                  <p className="text-[1.2rem]">Lingerie & Nightwear (35%)</p>
+                                                  <p className="text-[0.6rem]">2,345 Category Products</p>
+                                                </div>
+                                              </span>
+                                            </div>
+                                      </div>
+                                      <div className="w-[20%]">
+                                        <p>$2, 280</p>
+                                      </div>
+                                  </div>
+                            </div>
+
+                            
+                      </div>
+                      
               </div>
         </div>
     </div>
